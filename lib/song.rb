5153077157ac5d7c1927f4ac4d52145e30f4921e
Sarah Harrs <sarahharrs@gmail.com>
2019-07-17 00:29:26 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,44 +10,61 @@ class Song
     self.class.all << self
   end
 
-  def self.create(name)
-    @name=name
-    @@all<<self
+  def self.create
+    song = Song.new
+    song.save   # is this another method for @@all <<self?
+    song   # puts song
   end
 
-def self.new_by_name(name)
-    self.all.each{|name| puts "#{song.name}"}
+def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song   #puts song
   end
 end
 
-def self.create_by_name(name)
-  puts self.new_by_name
-  @@all <<self
-end
-
+def self.create_by_name(song_name)
+   song = self.create
+   song.name = song_name
+   song
+ end
 
   def self.find_by_name(name)
-     @@all.find{|name| song.name == name}
+     self.all.detect{|s| s.name == song_name}
    end
 
    def self.find_or_create_by_name
+   self.find_by_name(song_name) || self.create_by_name(song_name)
    end
 
   def self.alphabetical
-    @@all.sort_by.alphabetical
+    self.all.sort_by{|s|s.name}
   end
 
-  def sef.new_from_filename
-    self.split
-end
+  def self.new_from_filename(filename)
+    parts = filename.split ("-")
+    artist_name = parts[0]
+    song_name = parts[1].gsub (".mp3","")
 
-def self.create_from_filename
-end
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
 
+def self.create_from_filename(filename)
+  parts = filename.split ("-")
+  artist_name= parts[0]
+  song_name = parts[1].gsub(".mp3", "")
+
+song = self.create
+song.name = song_name
+song.artist_name = artist_name
+song
 end
 
   def self.destroy_all
-    @@all.clear
+    self.all.clear
   end
 
 end
