0b6879ab28286d8e0f1591ae08176cbbe964becc
Sarah Harrs <sarahharrs@gmail.com>
2019-07-17 00:12:31 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,44 @@ class Song
     self.class.all << self
   end
 
+  def self.create(name)
+    @name=name
+    @@all<<self
+  end
+
+def self.new_by_name(name)
+    self.all.each{|name| puts "#{song.name}"}
+  end
+end
+
+def self.create_by_name(name)
+  puts self.new_by_name
+  @@all <<self
+end
+
+
+  def self.find_by_name(name)
+     @@all.find{|name| song.name == name}
+   end
+
+   def self.find_or_create_by_name
+   end
+
+  def self.alphabetical
+    @@all.sort_by.alphabetical
+  end
+
+  def sef.new_from_filename
+    self.split
+end
+
+def self.create_from_filename
+end
+
+end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
 end
